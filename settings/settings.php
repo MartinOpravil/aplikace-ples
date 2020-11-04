a001b9a2eba56f2cfc26c9888428de657b1bb18a
Martin Opravil <m.opravil@gmail.com>
2020-11-04 19:31:39 +0100

    added centralized settings file

---
+++ settings/settings.php
@@ -0,0 +1,13 @@
+<?php
+    /* Nastavení pro Aplikaci Ples */
+    // Upravujte prosím pouze číselné hodnoty !!!
+
+    // Maximální počet vybraných sedadel pro osobu - Default [4]
+    $max_number_of_selected_seats = 4;
+
+    // Cena za jeden lístek v Kč - Default [150]
+    $cost_per_ticket = 150;
+
+    // Délka vygenerovaného hesla - Default [6]
+    $password_lenght = 6;
+?>
